455fc9236d0b1a9a33f1a26440881e20c217b0d5
MHajdu <mozes.hajdu@dmgmori-heitec.hu>
2022-05-08 10:07:30 +0200

    initial commit

---
+++ src/main/java/com/mozeshajdu/audiotagmanager/AudioTagManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mozeshajdu.audiotagmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTagManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTagManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozeshajdu/audiotagmanager/AudioTagManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozeshajdu.audiotagmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTagManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
